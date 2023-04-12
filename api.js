9cb85f6986362824ce4abdece6ecedcd77f4bf64
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-04-13 03:45:48 +0600

    loading

--- api.js
+++ api.js
@@ -9,6 +9,19 @@ const displaySingleUser = user =>{
     // console.log(user);
 }
 
+
+// meal db
+
+const toggoleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
+const searchMeal = () =>{
+    const searchText = document.getElementById('search-field').value;
+    loadMeals(searchText);
+    document.getElementById('search-field').value = '';
+}
+
 const loadMeals = searchText =>{
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
@@ -19,12 +32,13 @@ const loadMeals = searchText =>{
 const displayMeals = meals =>{
     
     const container = document.getElementById('meals');
+    container.textContent = '';
     meals.forEach(meal => {
         console.log(meal);
        const div = document.createElement('div');
        div.innerHTML = `
        <h1>${meal.strMeal}</h1>
-       <button onclick="loadMealDetail">Click me</button>
+       <button onclick="loadMealDetail('${meal.strMeal}')">Click me</button>
        `;
        container.appendChild(div)
     });
